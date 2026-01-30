a3603be28ee5fb8114cb466950666bd46a3da818
felisarmp4 <felisayereyes@gmail.com>
2026-01-30 14:03:37 -0500

    hail big

--- create-task/src/main.js
+++ create-task/src/main.js
@@ -1,8 +1,14 @@
 import "./style.css";
 const startscreen = document.querySelector(".startscreen");
 const playbtn = document.querySelector(".playbtn");
+const gamebuttons = document.querySelector(".gamebuttons");
 
 playbtn.addEventListener("click", () => {
-  startscreen.style.display = "none";
-  startgame();
+  startscreen.classList.add("fade-out");
+
+  setTimeout(() => {
+    startscreen.style.display = "none";
+    gamebuttons.classList.remove("hidden");
+    startgame();
+  }, 400);
 });
